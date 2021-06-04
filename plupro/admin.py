648b4134928cd7cb78c4a1a163db04c917a6440a
Anthony <theanthonyplan@gmail.com>
2021-06-04 03:56:29 -0400

    more model updates

--- plupro/admin.py
+++ plupro/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
-from .models import Proposal, ProposalLine, ProposalLineCategory
+from .models import Proposal, ProposalLine, ProposalLineCategory, ProposalLineItem
+
+class ProposalLineItemTabularInline(admin.TabularInline):
+    model = ProposalLineItem
+    extra = 1
+
 
 class ProposalLineTabularInline(admin.TabularInline):
     model = ProposalLine
+    inlines = [ProposalLineItemTabularInline]
+    extra = 1
+    ordering = ['quantity', 'override','proposal']
+
+
+class ProposalLineItemAdmin(admin.ModelAdmin):
+    model = ProposalLineItem
+    # list_display = ['quantity', 'category','override','proposal']
+
 
 class ProposalLineAdmin(admin.ModelAdmin):
     model = ProposalLine
-    list_display = ['quantity', 'override', 'category','proposal']
+    list_display = ['quantity', 'override','proposal', ]
 
 
 class ProposalAdmin(admin.ModelAdmin):
@@ -19,3 +33,4 @@ class ProposalLineCategoryAdmin(admin.ModelAdmin):
 admin.site.register(Proposal, ProposalAdmin)
 admin.site.register(ProposalLine, ProposalLineAdmin)
 admin.site.register(ProposalLineCategory, ProposalLineCategoryAdmin)
+admin.site.register(ProposalLineItem, ProposalLineItemAdmin)
--- plupro/models.py
+++ plupro/models.py
@@ -6,8 +6,13 @@ from django.db import models
 class Proposal(models.Model):
     # model field for the customer's name, should never be blank, max length of 60
     customer_name = models.CharField(blank=False, max_length=60)
-
-
+    technician_name = models.CharField(blank=False, max_length=60)
+    address = models.CharField(blank=False, max_length=120)
+    city_state_zip = models.CharField(blank=False, max_length=120)
+    phone_number = models.CharField(blank=False, max_length=20)
+    logo = models.ImageField(blank=True,max_length=100)
+    terms_and_conditions = models.TextField(blank=True)
+    technician_notes = models.TextField(blank=True)
     # this is how an object's name will be formatted
     def __str__(self):
             return self.customer_name
@@ -30,6 +35,27 @@ class ProposalLineCategory(models.Model):
             return str("Category: " + self.name)
 
 
+#model for proposal line item objects
+class ProposalLineItem(models.Model):
+    UM_CHOICES = [
+        ('EA', 'EA'),
+        ('FT', 'FT'),
+    ]
+
+    # model field for the customer's name, should never be blank, max length of 60
+    name = models.CharField(blank=False, max_length=60)
+    price = models.FloatField(blank=False, default=1.0)
+    um = models.CharField(blank=False, choices=UM_CHOICES, default='EA', max_length=2)
+    category = models.ForeignKey(ProposalLineCategory, blank=False, null=True, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Proposal Line Items"
+    def __str__(self):
+            return self.name
+    # this is how an object will represent itself (as a string)
+    def __repr__(self):
+            return str("LineItem: " + self.name)
+
+
 
 # model for proposal line objects
 class ProposalLine(models.Model):
@@ -37,25 +63,21 @@ class ProposalLine(models.Model):
     quantity = models.FloatField(blank=False, default=1.0)
     # field to override the default price of the line item object.  if blank, should be NULL
     override = models.FloatField(blank=True, null=True)
-    #
-    category = models.ForeignKey(ProposalLineCategory, null=True, on_delete=models.CASCADE)
-    #
+    # field to map the proposal line with a category
+    # category = models.ForeignKey(ProposalLineCategory, null=True, on_delete=models.CASCADE)
+    # field to map the proposal line with a proposal item
+    proposal_item = models.ForeignKey(ProposalLineItem, on_delete=models.CASCADE, blank=True, null=True)
+
+    # field to map the proposal line with a proposal
     proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE, blank=True, null=True)
+
 
     class Meta:
         verbose_name_plural = "Proposal Lines"
 
     # this is how an object will print its name (as a string)
     def __str__(self):
-            return "Proposal {}: Line #{}".format(self,proposal, self.pk)
+            return "Proposal {}: Line {}".format(self.proposal, self.pk)
     # this is how an object will represent itself (as a string)
     def __repr__(self):
-            return "Proposal {}: Line #{}".format(self,proposal, self.pk)
-
-
-
-
-# model for proposal objects
-# class ProposalLineItem(models.Model):
-#     # model field for the customer's name, should never be blank, max length of 60
-#     customer_name = models.CharField(blank=False, max_length=60)
+            return "Proposal {}: Line {}".format(self.proposal, self.pk)
